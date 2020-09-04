dd7a27d4ec498e773866f32b56a146e579b51693
Gabriel Khachatryan <gmkhach@gmail.com>
2020-09-03 23:16:45 -0400

    panic and fatal

--- printingAndLoggingInGo.go
+++ printingAndLoggingInGo.go
@@ -7,6 +7,15 @@ import (
 )
 
 func main() {
+    // Recover is another built-in function of the log package that helps you to regain control over the panicinc goroutine.
+    // If a goroutin in panicin you can call recover to capture the value given to panic and continue normal execution.
+    // Recover can only be used inside defered functions.
+    defer func(){
+        if r := recover(); r != nil {
+            fmt.Println("Recovered")
+        }
+    }()
+
     // With log you can are not limited to printing out just to standard out. 
     // For example, you can log your errors into a log file.
     _, err := os.Open("thisFileDoesNotExist.txt")
@@ -35,7 +44,14 @@ func main() {
     if err != nil {
         log.Println("Error:", err)
 
-        // The Fatal function will write the error in the logFile and call an os.Exit(1), which terminates the programs without making any of the deferred calls.
+        // Panic stops normal execution of the current goroutine and the execution of the cuntion is terminated.
+        // After it is returned to the caller of the function all of the defered functions are ran and then the execution of the caller is terminated.
+        // This process condinues cascading up until all function in the current go routine have stopped, and the program is terminated.
+        log.Panicln(err)
+        // You can also invoke panic by simply calling it directly and passing the error to it. 
+        // panic(err)
+
+        // The Fatal functionj will write the error in the logFile and call an os.Exit(1), which terminates the programs without making any of the deferred calls.
         log.Fatalln("Fatal Error:", err, "Exiting the program.")
     }
 }
